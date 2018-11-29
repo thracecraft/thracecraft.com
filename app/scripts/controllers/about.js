cb0afa94f18319b0e210c1273900f0ecd9124b0c
Rob Thijssen <rthijssen@gmail.com>
2018-11-29 13:04:33 +0200

    angular scaffold

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thracecraftcomApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the thracecraftcomApp
+ */
+angular.module('thracecraftcomApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thracecraftcomApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the thracecraftcomApp
+ */
+angular.module('thracecraftcomApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
